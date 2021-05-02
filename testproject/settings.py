f48bf1576c3dfd6cb76069de64786ce61c572442
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2021-05-02 13:35:19 +0600

    updates

--- testproject/settings.py
+++ testproject/settings.py
@@ -137,16 +137,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-'''
+
 AWS_ACCESS_KEY_ID = 'AKIARVGPJVYVATR5L7HQ'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'bucketeer-877dd773-51d8-45f1-bff8-64ae71ebc53d'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-'''
-
-DROPBOX_OAUTH2_TOKEN = 'sl.AwCE_ezGIKTgt0Bx-3R3q2e23YkpnsbRrB19rIoo57cGNfSl_pLtoFMJy2b71PG-vR1glVUhjT7fLXdVK7r1N6E053mZlu0vgbH4RYo3YI3B0QCTUOEkEKW4XcEO-ASq9_LfVKA'
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
