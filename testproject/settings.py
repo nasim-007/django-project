10a611dca6398955eecbeb9cb0693203037d552f
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2021-05-02 15:02:32 +0600

    updates

--- testproject/settings.py
+++ testproject/settings.py
@@ -135,16 +135,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'static/media/'
 
-
+'''
 AWS_ACCESS_KEY_ID = 'AKIARVGPJVYVATR5L7HQ'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'bucketeer-877dd773-51d8-45f1-bff8-64ae71ebc53d'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+'''
 
 
 # Default primary key field type
