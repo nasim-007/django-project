d1e6c62fa378e68c3bd6d03a8efb7ebd7e7cb88f
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2021-05-03 23:46:27 +0600

    update blogs

--- testproject/urls.py
+++ testproject/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls', namespace='myapp')),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('album/', include('album.urls', namespace='album'))
 
 
