16eb623fc90b7a5380781339b94f115c293ee386
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2021-05-03 23:12:08 +0600

    update blueberry

--- album/urls.py
+++ album/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'album'
 
 urlpatterns = [
-    path('', views.album, name='album')
+    path('', views.album, name='album'),
+    path('blueberry/', views.blueberry, name='blueberry')
 ]
