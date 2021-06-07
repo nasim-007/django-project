947656677ec25b0eced2844e4fa6dd0b48aaf047
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2021-06-07 15:31:36 +0600

    updates

--- testproject/settings.py
+++ testproject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s@4jvn1kp+^83ret-y6=48bsqtun*o2$d!^i96mpz-j+y6_0)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
